a793385bbbf34322313bff4f576a7c336d1cc769
Rodin Igor <igormzk91@gmai.com>
2022-03-31 21:28:49 +0700

    Доделал задание, проверка гита

--- t2est.py
+++ t2est.py
@@ -1,7 +1,8 @@
 
 count_n = int(input('Кол-во чисел в списке: '))
 list_num = []
-
+count_i = []
+summ_i = 0 
 for num in range(1, count_n + 1):
     number = int(input(f'Ведите {num} число: '))
     list_num.append(number)
@@ -10,4 +11,10 @@ div_num = int(input('Введите делитель: '))
 
 for div in list_num:
     if div % div_num == 0:
-        print(f'Индекс числа {div}: ') 
+        count_i.append(div)
+        print(f'Индекс числа {div}: {len(count_i)}')
+
+for index in range(len(count_i)):
+    summ_i = index + 1 + summ_i   
+
+print(f'Сумма индексов: {summ_i}')
