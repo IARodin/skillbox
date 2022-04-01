72f746604324cba6406e933fb420162603649a09
Rodin <igormzk91@gmail.com>
2022-04-01 21:11:55 +0300

    Дз модуль 14 задание 4

--- test.py
+++ test.py
@@ -1,52 +1,30 @@
-def payment(starting_credit, years, percent):
-  coefficient = percent * (1 + percent) ** years / ((1 + percent) ** years - 1)
-  return round(coefficient * starting_credit, 2)
 
+n = input('Введите первое число: ')
+k = input('Введите второе число: ')
+c1 = 'Первое'
+c2 = 'Второе'
 
-def percent_paid(credit, percent):
-  return credit * percent
+def reverse(number, c):
+  a = ''
+  b = ''
+  flag = False
+  for symbol in number:
+    if symbol != '.':
+      a = symbol + a
+    elif symbol == '.':
+      break
+  for symbol in number:
+    if flag == True:
+      b = symbol + b
+    if symbol == '.':
+      flag = True
+  print(f'{c} число наоборот: {a}.{b} ')
 
-
-def credit_paid(starting_credit, credit, years, percent):
-  return payment(starting_credit, years, percent) - percent_paid(credit, percent)
-
-
-def debt(starting_credit, credit, years, percent):
-  return credit - credit_paid(starting_credit, credit, years, percent)
-
-
-def show_results(starting_credit, credit, years, percent, period):
-  print('\nПериод:', period, end = '\n\n')
-  print('Остаток долга на начало периода:',  credit)
-  print('Выплачено процентов:', percent_paid(credit, percent))
-  print('Выплачено тела кредита:', credit_paid(starting_credit, credit, years, percent))
-
-
-credit = float(input('Введите сумму кредита: '))
-years = int(input('На сколько лет выдан? '))
-percent = float(input('Сколько процентов годовых? '))
-
-starting_credit = credit
-
-period = 1
-while period < 4:
-  show_results(starting_credit, credit, years, percent / 100, period)
-  credit = debt(starting_credit, credit, years, percent / 100)
-  period += 1
-
-print('\nОстаток долга:', credit)
-print('\n\n', '=' * 30, '\n\n')
-
-years_prolongation = int(input('На сколько лет продляется договор? '))
-percent_increase = float(input('Увеличение ставки до: '))
-
-years = years - 3 + years_prolongation
-starting_credit = credit
-
-period = 1
-while period <= years:
-  show_results(starting_credit, credit, years, percent_increase / 100, period)
-  credit = debt(starting_credit, credit, years, percent_increase / 100)
-  period += 1
-
-print('\nОстаток долга:', credit)
+# TODO оформить код по правилам PEP8
+reverse(n, c1)
+reverse(k, c2)
+# TODO дополнить ответ выводом суммы полученных "наоборот" чисел:
+# Введите первое число: 12.45
+# Введите второе число: 34.54
+# Первое число наоборот: 21.54
+# Второе число наоборот: 43.45
